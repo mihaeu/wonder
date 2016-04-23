8a68080cf5329da289d35d9c0ae282e246417326
Michael Haeuslmann <haeuslmann@gmail.com>
2016-04-23 08:30:34 +0200

    Add space between words of name

--- core/src/main/java/wonder/core/Cards/GuardTower.java
+++ core/src/main/java/wonder/core/Cards/GuardTower.java
@@ -7,6 +7,6 @@ import static wonder.core.Resources.Type.Clay;
 
 public class GuardTower extends Card {
     public GuardTower(int minPlayers) {
-        super("GuardTower", Type.Red, Age.One, minPlayers, ResourcePool.cost(Clay));
+        super("Guard Tower", Type.Red, Age.One, minPlayers, ResourcePool.cost(Clay));
     }
 }
